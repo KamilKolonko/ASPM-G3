ebd1f46052b48b3e352218f61b30a2231946011f
KamilKolonko <k.kolonko@outlook.com>
2015-11-22 18:52:06 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("This is a test message");
+        System.out.println("Test");
+        System.out.println("Test");
     }
 }
