65e9d49b3d15692b8f4bc4eb59296333609dcdcf
caoqian <caoqian@192.168.1.103>
2015-12-01 16:23:31 +0100

    model

---
+++ src/model/MusicModel.java
@@ -0,0 +1,14 @@
+package model;
+
+public class MusicModel {
+
+	private static String mode="default";//default, random,  one song circle, list circle , play one song  
+
+	public static String getMode() {
+		return mode;
+	}
+
+	public static void setMode(String mode) {
+		MusicModel.mode = mode;
+	}
+}
